dfa5f3c2fd7468724b8e25296112810e1d29c048
Jorge Garcia <info@jorgegarciamartin.com>
2019-01-24 12:18:34 +0100

    TinyTasksPool WIP

--- include/tinytasks.h
+++ include/tinytasks.h
@@ -1,7 +1,9 @@
 #ifndef __TINYTASKS_H__
 #define __TINYTASKS_H__
 
+#include <cassert>
 #include <string>
+#include <atomic>
 
 #define TINYTASKS_VERSION_MAJOR 1
 #define TINYTASKS_VERSION_MINOR 0
@@ -20,7 +22,36 @@ static const char* version()
     
     return version.c_str();
 }
+    
+namespace constants
+{
+    static const uint8_t kMaxNumThreadsInPool = UINT8_MAX;
+    static const uint8_t kMinNumThreadsInPool = 2;
+}
+    
+class NonCopyableMovable
+{
+public:
+    explicit NonCopyableMovable() {}
+protected:
+    NonCopyableMovable(const NonCopyableMovable&) = delete;
+    NonCopyableMovable& operator=(const NonCopyableMovable&) = delete;
+    NonCopyableMovable(NonCopyableMovable&&) = delete;
+    NonCopyableMovable& operator=(NonCopyableMovable&&) = delete;
+};
 
+class TinyTasksPool : public NonCopyableMovable
+{
+public:
+    explicit TinyTasksPool() : m_numThreads(constants::kMinNumThreadsInPool) {}
+    TinyTasksPool(uint8_t numThreads) : m_numThreads(numThreads) {}
+    
+    uint8_t GetNumThreads() const { return m_numThreads; }
+
+private:
+    uint8_t m_numThreads;
+};
+    
 }
 
 #endif
--- test/tests.cpp
+++ test/tests.cpp
@@ -8,3 +8,25 @@ TEST(TinyTasksTest, TestLibVersionNumber)
     ASSERT_STREQ(tinytasks::version(), "1.0.0");
 }
 
+TEST(TinyTasksTest, TestCreateTinyTasksPoolDefault)
+{
+    TinyTasksPool tinyTasksPool;
+    
+    ASSERT_EQ(tinyTasksPool.GetNumThreads(), tinytasks::constants::kMinNumThreadsInPool);
+}
+
+TEST(TinyTasksTest, TestCreateTinyTasksPoolNonDefault)
+{
+    const uint8_t numThreads = 20;
+    TinyTasksPool tinyTasksPool(numThreads);
+    
+    ASSERT_EQ(tinyTasksPool.GetNumThreads(), numThreads);
+}
+
+TEST(TinyTasksTest, TestCreateTinyTasksPoolNonDefaultMax)
+{
+    const uint8_t numThreads = tinytasks::constants::kMaxNumThreadsInPool;
+    TinyTasksPool tinyTasksPool(numThreads);
+    
+    ASSERT_EQ(tinyTasksPool.GetNumThreads(), numThreads);
+}
