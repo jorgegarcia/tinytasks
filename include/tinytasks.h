65803ae5f8daafdd76b3fbfa15338b247b164ef7
Jorge Garcia <info@jorgegarciamartin.com>
2019-01-27 17:34:10 +0100

    Added header with copyright and MIT license to the library

--- include/tinytasks.h
+++ include/tinytasks.h
@@ -1,6 +1,39 @@
 #ifndef __TINYTASKS_H__
 #define __TINYTASKS_H__
 
+//
+//     _____  _               _____             _
+//    /__   \(_) _ __   _   _/__   \ __ _  ___ | | __ ___
+//      / /\/| || '_ \ | | | | / /\// _` |/ __|| |/ // __|
+//     / /   | || | | || |_| |/ /  | (_| |\__ \|   < \__ \
+//     \/    |_||_| |_| \__, |\/    \__,_||___/|_|\_\|___/
+//                      |___/
+//
+//    Simple and single-header tasks library in C++11
+//
+//    The MIT License (MIT)
+//
+//    Copyright (c) 2019 Jorge Garcia Martin
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in
+//    all copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//    THE SOFTWARE.
+//
+
 #include <cassert>
 #include <memory>
 #include <string>
@@ -16,7 +49,7 @@
 #define TINYTASKS_VERSION_MINOR 0
 #define TINYTASKS_VERSION_PATCH 0
 
-#define TINYTASKS_SAFE_DELETE(p)       if(p) { delete p;   p = nullptr; }
+#define TINYTASKS_SAFE_DELETE(p) if(p) { delete p; p = nullptr; }
 
 namespace tinytasks
 {
